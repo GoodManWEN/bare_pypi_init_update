1420832e262d8c293c668782e8ed54c6c95823b3
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-03-07 07:55:34 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -128,10 +128,14 @@ headers = {{
 html = BeautifulSoup(rget(url , headers).text ,'lxml')
 description = html.find('meta' ,{{'name':'description'}}).get('content')
 html = BeautifulSoup(rget(release , headers).text ,'lxml')
-version = html.find('div',{{'class':'release-header'}}).find('a').text
+logger.info(f"description: {{description}}")
+
+#
+with open('tagname','r',encoding='utf-8') as f:
+    version = f.read()
 if ':' in version:
     version = version[:version.index(':')].strip()
-logger.info(f"description: {{description}}")
+version = version.strip()
 logger.info(f"version: {{version}}")
 
 #
